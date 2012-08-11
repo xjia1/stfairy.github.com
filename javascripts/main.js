99045db4535f94e32db32365046d08cbd75dc2b3
Xiao Jia <stfairy@gmail.com>
2012-08-11 08:30:53 +0800

    refine

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+$(function(){
+  $.get('index.md', function(text){
+    var converter = new Showdown.converter();
+    var html = converter.makeHtml(text);
+    $('#main_content').html(html);
+  });
+});
